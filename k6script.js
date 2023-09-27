e905e5eb48655b83a1a636a11b97b95ad6dcb9ca
frankhung780415 <145753690+frankhung780415@users.noreply.github.com>
2023-09-27 14:13:40 +0800

    Update k6script.js

--- k6script.js
+++ k6script.js
@@ -1,5 +1,12 @@
 import http from 'k6/http';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export default function () {
   http.get('https://test.k6.io');
+}
+
+export function handleSummary(data) {
+  return {
+    "summary.html": htmlReport(data),
+  };
 }
